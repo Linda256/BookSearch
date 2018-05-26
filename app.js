c331e56492df3355214719aebcb7b3ff9d6223b9
Linda Wang <lindawang256@gmail.com>
2018-05-25 17:22:49 -0700

    connected to mongodb

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const mongoose = require ('mongoose');
+
+mongoose.connect('mongodb://localhost/booksdb');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'connection error: '))
+
+db.once('open',function(){
+  console.log('booksdb is connected!!')
+})
+
+
 app.get('/', (req,res) => {
   res.send('Find you farorite book here!!!')
 })
+
+//app.get()
 
 let port = 8080;
 app.listen(port, function () {
