80a2da84b7d15decdc2ee1af2e1f07484f2f2add
Linda Wang <lindawang256@gmail.com>
2018-05-29 19:06:39 -0700

    add books

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -14,8 +14,28 @@ class Books extends Component {
     .then(books => this.setState({books},() => console.log('Books fetched...',books)));
   }
   render() {
+      let categoryOptions = this.state.books.map(book => {
+             return <option key={book._id}  value = ' category'> {book.genre}</option>
+          });
+
     return (
       <div >
+          <div>
+            <label>Title</label><br />
+            <input type='text' ref ='title' />
+          </div>
+          <div>
+            <label>Author</label><br />
+            <input type='text' ref ='author' />
+          </div>
+          <div>
+            <label>Genre</label><br />
+            <select ref ='category' >
+              {categoryOptions}
+            </select>
+          </div>
+          <input type='submit' value ='Search' />
+
           <h2>Books</h2>
           <ul>
           {this.state.books.map(book => <li key={book._id}> <strong> {book.title} </strong>  : {book.author}</li>)}
